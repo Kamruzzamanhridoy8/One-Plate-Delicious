f742a56473b4e4fe5cfc5ffc9b4361d58f753126
Kamruzzamanhridoy8 <Kamruzzamanhridoy@hotmail.com>
2023-03-27 00:27:36 +0600

    adding modal

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ const getmeals= (MealNmae ='')=>{
         .then(res=>res.json())
         .then(data=>displayMeals(data.meals))
 }
-
+// onclick="CallMealsDetails(${element.idMeal})"
 const displayMeals= meals =>{
     const mealsPusher = document.getElementById('mealspusher')
     mealsPusher.innerHTML =''
@@ -13,11 +13,12 @@ const displayMeals= meals =>{
         // console.log(element.strMealThumb);
         creatDiv.classList.add('col')
         creatDiv.innerHTML = `
-        <div onclick="CallMealsDetails(${element.idMeal})" class="card mealCard">
+        <div data-bs-toggle="modal" data-bs-target="#exampleModal" class="card mealCard" >
             <img src="${element.strMealThumb}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">${element.strMeal}</h5>
                 <p class="card-text">${element.strInstructions.slice(0,90)}...</p>
+                
             </div>
         </div>
         `;
@@ -28,17 +29,37 @@ const displayMeals= meals =>{
 const searchBox =()=>{
     const searchbox= document.getElementById('searchbox')
     const boxvalue = searchbox.value;
-    console.log(boxvalue);
     getmeals(boxvalue)
     searchbox.value=''
 
 }
 
+searchbox.addEventListener("keypress", function(event) {
+    const searchbox= document.getElementById('searchbox')
+    const boxvalue = searchbox.value;
+    console.log(event.key);
+    if (event.key === "Enter") {
+        getmeals(boxvalue);
+    }
+});
+
+
 const CallMealsDetails = (mealid)=>{
     const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealid}`
     fetch(url)
         .then(res=>res.json())
-        .then(data=>console.log(data))
+        .then(data=>displayMealsDetails(data))
     
 }
+
+const displayMealsDetails = mealid =>{
+    const mealmodel = document.getElementById('mealModel');
+    mealmodel.innerHTML =`
+
+    `
+}
+
+
+
+
 getmeals()
